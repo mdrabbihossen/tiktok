aca0e096a214cdf97a5c18023157a993bc198734
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-01-08 21:16:57 +0600

    login view started

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,19 +2,26 @@ import 'package:get/get.dart';
 
 import 'package:tiktok/app/modules/home/bindings/home_binding.dart';
 import 'package:tiktok/app/modules/home/views/home_view.dart';
+import 'package:tiktok/app/modules/login/bindings/login_binding.dart';
+import 'package:tiktok/app/modules/login/views/login_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.LOGIN;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
     ),
   ];
 }
