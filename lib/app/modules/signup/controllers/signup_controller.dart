2656ade843a90e03ca33b5a88abb773a29cc5d86
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-01-10 15:31:45 +0600

    signup complete

--- lib/app/modules/signup/controllers/signup_controller.dart
+++ lib/app/modules/signup/controllers/signup_controller.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tiktok/app/utils/firebase.dart';
 import '../../../data/user_model.dart' as model;
 
@@ -22,10 +23,24 @@ class SignupController extends GetxController {
     showPassword.value = !showPassword.value;
   }
 
+  // picked image
+  onImagePicked() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedImage != null) {
+      Get.snackbar(
+        "Profile Picture",
+        "You have successfully selected your profile picture!",
+      );
+    }
+    profilePhoto.value = File(pickedImage!.path);
+  }
+
+  // picked image end
   // register account
   registerUser() async {
     showLoading.value = true;
-
     try {
       if (usernameController.text.isNotEmpty &&
           emailController.text.isNotEmpty &&
@@ -35,32 +50,44 @@ class SignupController extends GetxController {
           email: emailController.text,
           password: passwordController.text,
         );
-        String downloadUrl = await _uploadToStorage(profilePhoto.value);
+        String downloadUrl = await _uploadToStorage();
         model.User user = model.User(
           name: usernameController.text,
           email: emailController.text,
           profilePhoto: downloadUrl,
           uid: credential.user!.uid,
         );
-
+        await FirebaseUtils.fireStore
+            .collection('users')
+            .doc(credential.user!.uid)
+            .set(
+              user.toJson(),
+            );
+      } else {
+        Get.snackbar(
+          'Error',
+          'Please fill all the fields',
+          snackPosition: SnackPosition.BOTTOM,
+        );
       }
     } catch (e) {
       Get.snackbar("Error Creating account", e.toString());
+      print(e.toString());
     }
+    showLoading.value = false;
   }
 
   // register account end
 
   // upload to storage
-  Future<String> _uploadToStorage(File image) async {
+  Future<String> _uploadToStorage() async {
     Reference ref = FirebaseUtils.firebaseStorage
         .ref()
-        .child('profilePics')
+        .child('profilePhotos')
         .child(FirebaseUtils.firebaseAuth.currentUser!.uid);
-    UploadTask uploadTask = ref.putFile(image);
-    TaskSnapshot snapshot = await uploadTask;
-    String downloadUrl = await snapshot.ref.getDownloadURL();
-    return downloadUrl;
+    UploadTask uploadTask = ref.putFile(profilePhoto.value);
+    TaskSnapshot taskSnapshot = await uploadTask;
+    return await taskSnapshot.ref.getDownloadURL();
   }
 // upload to storage end
 }
--- lib/app/modules/signup/views/signup_view.dart
+++ lib/app/modules/signup/views/signup_view.dart
@@ -13,129 +13,135 @@ class SignupView extends GetView<SignupController> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Welcome to Tiktok',
-            style: TextStyle(
-              fontSize: 35,
-              color: AppColors.buttonColor,
-              fontWeight: FontWeight.w900,
-            ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Sign Up',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          SizedBox(height: 25),
-          Stack(
-            children: [
-              const CircleAvatar(
-                radius: 64,
-                backgroundImage: CachedNetworkImageProvider(
-                    'https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png'),
-                backgroundColor: Colors.black,
-              ),
-              Positioned(
-                bottom: -10,
-                left: 80,
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.add_a_photo,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 15),
-          Form(
-            key: controller.formKey,
-            child: Column(
-              children: [
-                Container(
-
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: AuthTextField(
-                    controller: controller.usernameController,
-                    labelText: "Username",
-                    prefixIcon: Icons.person,
-                    errorText: "Username Required",
-                  ),
-                ),
-                SizedBox(height: 15),
-                Container(
-
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: AuthTextField(
-                    controller: controller.emailController,
-                    labelText: "Email",
-                    prefixIcon: Icons.email_rounded,
-                    errorText: "Email Required",
-                  ),
-                ),
-                SizedBox(height: 15),
-                Obx(
-                  () => Container(
-
-                    margin: EdgeInsets.symmetric(horizontal: 20),
-                    child: AuthTextField(
-                      controller: controller.passwordController,
-                      labelText: "Password",
-                      prefixIcon: Icons.lock_outline_rounded,
-                      isPassword: true,
-                      errorText: "Password Required",
-                      showPassword: controller.showPassword.value,
-                      showPasswordPressed: () {
-                        controller.onVisibilityChange();
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 30),
-          Obx(
-            () => controller.showLoading.value
-                ? CircularProgressIndicator()
-                : AuthButton(
-                    size: size,
-                    authText: "SignUp",
-                    onPress: () {
-                      if (controller.formKey.currentState!.validate()) {}
-                    },
-                  ),
-          ),
-          SizedBox(height: 15),
-          Row(
+      body: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'Already have an account?',
+                'Welcome to Tiktok',
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 35,
+                  color: AppColors.buttonColor,
+                  fontWeight: FontWeight.w900,
                 ),
               ),
-              SizedBox(width: 10),
-              GestureDetector(
-                onTap: () => Get.back(),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: AppColors.buttonColor,
+              SizedBox(height: 10),
+              Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 25),
+              Stack(
+                children: [
+                  const CircleAvatar(
+                    radius: 64,
+                    backgroundImage: CachedNetworkImageProvider(
+                        'https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png'),
+                    backgroundColor: Colors.black,
                   ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: () {
+                        controller.onImagePicked();
+                      },
+                      icon: Icon(
+                        Icons.add_a_photo,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Form(
+                key: controller.formKey,
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      child: AuthTextField(
+                        controller: controller.usernameController,
+                        labelText: "Username",
+                        prefixIcon: Icons.person,
+                        errorText: "Username Required",
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      child: AuthTextField(
+                        controller: controller.emailController,
+                        labelText: "Email",
+                        prefixIcon: Icons.email_rounded,
+                        errorText: "Email Required",
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Obx(
+                      () => Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        child: AuthTextField(
+                          controller: controller.passwordController,
+                          labelText: "Password",
+                          prefixIcon: Icons.lock_outline_rounded,
+                          isPassword: true,
+                          errorText: "Password Required",
+                          showPassword: controller.showPassword.value,
+                          showPasswordPressed: () {
+                            controller.onVisibilityChange();
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
+              SizedBox(height: 30),
+              Obx(
+                () => controller.showLoading.value
+                    ? CircularProgressIndicator()
+                    : AuthButton(
+                        size: size,
+                        authText: "Sign Up",
+                        onPress: () {
+                          if (controller.formKey.currentState!.validate()) {
+                            controller.registerUser();
+                          }
+                        },
+                      ),
+              ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account?',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: () => Get.back(),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: AppColors.buttonColor,
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
